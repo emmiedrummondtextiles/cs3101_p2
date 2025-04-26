3a29edc6f5d99840b9a9d87f03d9ac29e5d98961
emmiedrummondtextiles <166390691+emmiedrummondtextiles@users.noreply.github.com>
2025-04-26 18:40:46 +0100

    Update views.sql

--- sql/views.sql
+++ sql/views.sql
@@ -1,102 +1,45 @@
--- views.sql: define the three required views
+-- simplified view definitions matching lecture style
 
--- 1. Services on which train 170406 operates (trainLEV)
-CREATE OR REPLACE VIEW trainLEV AS
-SELECT
-    s.hc,
-    r.orig   AS orig,
-    MAKETIME(s.dh, s.dm, 0) AS dep
-FROM service s
-JOIN route r USING (hc)
-WHERE s.uid = '170406'
-ORDER BY dep;
+create view trainLEV as
+ select s.hc, stn.code as orig, maketime(s.dh,s.dm,0) as dep
+ from service s
+ join route r using(hc)
+ join station stn on stn.loc = r.orig
+ where s.uid = '170406';
 
--- 2. Services departing Edinburgh (EDB) (scheduleEDB)
-CREATE OR REPLACE VIEW scheduleEDB AS
-SELECT
-    s.hc,
-    MAKETIME(s.dh, s.dm, 0)                              AS dep,
-    st.pl                                                 AS pl,
-    (
-      SELECT p2.loc
-      FROM plan p2
-      WHERE p2.hc = s.hc
-        AND p2.frm = 'EDB'
-      ORDER BY p2.ddh, p2.ddm
-      LIMIT 1
-    )                                                     AS dest,
-    (
-      SELECT COUNT(*)
-      FROM coach c
-      WHERE c.uid = s.uid
-    )                                                     AS len,
-    s.toc                                                 AS toc
-FROM service s
-JOIN route r USING (hc)
-LEFT JOIN stop st
-  ON st.hc = s.hc
- AND st.loc = 'EDB'
-WHERE r.orig = 'EDB'
-ORDER BY dep;
+create view scheduleEDB as
+ select s.hc,
+        maketime(s.dh,s.dm,0) as dep,
+        s.pl as pl,
+        (select p2.loc
+         from plan p2
+         where p2.hc = s.hc and p2.frm = 'EDB'
+         order by p2.ddh,p2.ddm limit 1) as dest,
+        (select count(*) from coach c where c.uid = s.uid) as len,
+        s.toc as toc
+ from service s
+ join route r using(hc)
+ where r.orig = 'EDB';
 
--- 3. Sequence of locations for the 18:59 1L27 service (serviceEDBDEE)
-CREATE OR REPLACE VIEW serviceEDBDEE AS
-SELECT locs.loc                                           AS loc,
-       stn.code                                          AS stn,
-       locs.pl                                            AS pl,
-       MAKETIME(locs.adh, locs.adm, 0)                   AS arr,
-       MAKETIME(locs.ddh, locs.ddm, 0)                   AS dep
-FROM (
-    -- origin
-    SELECT
-      r.orig                           AS loc,
-      NULL                             AS ddh,
-      NULL                             AS ddm,
-      NULL                             AS adh,
-      NULL                             AS adm,
-      NULL                             AS pl,
-      MAKETIME(s.dh, s.dm, 0)         AS dep_order
-    FROM service s
-    JOIN route r USING (hc)
-    WHERE s.hc = '1L27' AND s.dh = 18 AND s.dm = 59
-
-    UNION ALL
-
-    -- intermediate stops
-    SELECT
-      p.loc                            AS loc,
-      p.ddh                            AS ddh,
-      p.ddm                            AS ddm,
-      st.adh                           AS adh,
-      st.adm                           AS adm,
-      st.pl                            AS pl,
-      MAKETIME(p.ddh, p.ddm, 0)       AS dep_order
-    FROM plan p
-    JOIN stop st
-      ON st.hc = p.hc
-     AND st.loc = p.loc
-    WHERE p.hc = '1L27' AND NOT p.ddh IS NULL
-
-    UNION ALL
-
-    -- destination
-    SELECT
-      p2.loc                           AS loc,
-      NULL                             AS ddh,
-      NULL                             AS ddm,
-      st2.adh                          AS adh,
-      st2.adm                          AS adm,
-      NULL                             AS pl,
-      MAKETIME(st2.adh, st2.adm, 0)   AS dep_order
-    FROM plan p2
-    JOIN stop st2
-      ON st2.hc = p2.hc
-     AND st2.loc = p2.loc
-    WHERE p2.hc = '1L27'
-      AND p2.ddh IS NULL
-) AS locs
-LEFT JOIN station stn
-  ON stn.loc = locs.loc
-ORDER BY locs.dep_order;
+create view serviceEDBDEE as
+ select lo.loc as loc,
+        stn.code as stn,
+        lo.pl as pl,
+        maketime(lo.adh,lo.adm,0) as arr,
+        maketime(lo.ddh,lo.ddm,0) as dep
+ from (
+    select r.orig as loc,null as ddh,null as ddm,null as adh,null as adm,s.pl as pl
+    from service s join route r using(hc)
+    where s.hc='1L27' and s.dh=18 and s.dm=59
+    union all
+    select p.loc,p.ddh,p.ddm,st.adh,st.adm,st.pl
+    from plan p join stop st using(hc,loc)
+    where p.hc='1L27' and p.ddh is not null
+    union all
+    select p2.loc,null,null,st2.adh,st2.adm,null
+    from plan p2 join stop st2 using(hc,loc)
+    where p2.hc='1L27' and p2.ddh is null
+ ) as lo
+ left join station stn on stn.loc = lo.loc;
 
 
